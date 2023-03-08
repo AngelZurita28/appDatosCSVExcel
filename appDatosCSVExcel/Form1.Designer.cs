a498938b24ca660bce93375a1e85f2f2fe847af0
Angel Zurita <af3392976@gmail.com>
2023-03-08 08:46:56 -0600

    Add project files.

---
+++ appDatosCSVExcel/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace appDatosCSVExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.lstvDatos = new System.Windows.Forms.ListView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnExcelInterop = new System.Windows.Forms.Button();
+            this.btnExcelOpenXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(12, 12);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // lstvDatos
+            // 
+            this.lstvDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstvDatos.HideSelection = false;
+            this.lstvDatos.Location = new System.Drawing.Point(12, 76);
+            this.lstvDatos.Name = "lstvDatos";
+            this.lstvDatos.Size = new System.Drawing.Size(776, 362);
+            this.lstvDatos.TabIndex = 1;
+            this.lstvDatos.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardar.Location = new System.Drawing.Point(469, 12);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(113, 23);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Exportar texto plano";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnExcelInterop
+            // 
+            this.btnExcelInterop.Location = new System.Drawing.Point(588, 12);
+            this.btnExcelInterop.Name = "btnExcelInterop";
+            this.btnExcelInterop.Size = new System.Drawing.Size(89, 23);
+            this.btnExcelInterop.TabIndex = 3;
+            this.btnExcelInterop.Text = "Excel Interop";
+            this.btnExcelInterop.UseVisualStyleBackColor = true;
+            this.btnExcelInterop.Click += new System.EventHandler(this.btnGuardarExcel_Click);
+            // 
+            // btnExcelOpenXML
+            // 
+            this.btnExcelOpenXML.Location = new System.Drawing.Point(683, 12);
+            this.btnExcelOpenXML.Name = "btnExcelOpenXML";
+            this.btnExcelOpenXML.Size = new System.Drawing.Size(105, 23);
+            this.btnExcelOpenXML.TabIndex = 4;
+            this.btnExcelOpenXML.Text = "Excel OpenXml";
+            this.btnExcelOpenXML.UseVisualStyleBackColor = true;
+            this.btnExcelOpenXML.Click += new System.EventHandler(this.btnExcelOpenXML_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcelOpenXML);
+            this.Controls.Add(this.btnExcelInterop);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lstvDatos);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.ListView lstvDatos;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnExcelInterop;
+        private System.Windows.Forms.Button btnExcelOpenXML;
+    }
+}
+
---
+++ appDatosCSVExcel/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.Office.Interop.Excel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace appDatosCSVExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lstvDatos.View = System.Windows.Forms.View.Details;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogo = new OpenFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            { return; }
+            lstvDatos.Clear();
+            string rutaArchivo = dialogo.FileName;
+
+            StreamReader sr = new StreamReader(rutaArchivo, Encoding.GetEncoding(1252));
+            string columnas = sr.ReadLine();
+            string[] columna = columnas.Split('|');
+
+            for (int i = 0; i < columna.Length; i++)
+            { lstvDatos.Columns.Add(columna[i]); }
+
+            string renglon;
+            while ((renglon = sr.ReadLine()) != null)
+            {
+                string[] datos = renglon.Split('|');
+                ListViewItem item = new ListViewItem(datos[0]);
+
+                for (int i = 1; i < datos.Length; i++)
+                { item.SubItems.Add(datos[1]); }
+
+                lstvDatos.Items.Add(item);
+            }
+
+            sr.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Comma Separated Values | *.csv | Texto | *.txt | Excel | *.xlsx";
+
+            if (sfd.ShowDialog() != DialogResult.OK)
+            { return; }
+
+            StreamWriter sw = new StreamWriter(sfd.FileName);
+
+            foreach (ListViewItem item in lstvDatos.Items)
+            {
+                int i = 1;
+                foreach (ListViewItem.ListViewSubItem subItem in item.SubItems)
+                {
+                    if (i == lstvDatos.Columns.Count)
+                    {
+                        sw.Write(subItem.Text);
+                        sw.WriteLine();
+                        i = 0;
+                    }
+                    else
+                    { sw.Write(subItem.Text + ","); i++; }
+                }
+            }
+            sw.Close();
+            MessageBox.Show("Done");
+        }
+
+        private void btnGuardarExcel_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            if (sfd.ShowDialog() != DialogResult.OK)
+            { return; }
+
+            if (sfd.FileName != "")
+            {
+                Excel.Application excel = new Excel.Application();
+                Excel.Workbook workbook = excel.Workbooks.Add(Type.Missing);
+                Excel.Worksheet worksheet = null;
+
+                try
+                {
+                    worksheet = workbook.ActiveSheet;
+                    worksheet.Name = "ListView Data";
+
+                    int row = 1;
+                    int col = 1;
+
+                    foreach (ColumnHeader column in lstvDatos.Columns)
+                    {
+                        worksheet.Cells[row, col] = column.Text;
+                        col++;
+                    }
+                    row++;
+
+                    foreach (ListViewItem item in lstvDatos.Items)
+                    {
+                        col = 1;
+                        foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                        {
+                            worksheet.Cells[row, col] = subitem.Text;
+                            col++;
+                        }
+                        row++;
+                    }
+
+                    workbook.SaveAs(sfd.FileName);
+                    Process.Start(sfd.FileName);
+                }
+
+                catch (Exception ex)
+                { MessageBox.Show("Error: " + ex.Message); }
+
+                excel.Quit();
+                workbook = null;
+                excel = null;
+            }
+        }
+
+        private void btnExcelOpenXML_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            if (sfd.ShowDialog() != DialogResult.OK)
+            { return; }
+
+            if (sfd.FileName != "")
+            {
+                SpreadsheetDocument document = SpreadsheetDocument.Create(sfd.FileName, SpreadsheetDocumentType.Workbook);
+                // Agregar una hoja de trabajo
+                WorkbookPart workbookPart = document.AddWorkbookPart();
+                workbookPart.Workbook = new DocumentFormat.OpenXml.Spreadsheet.Workbook();
+                WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new DocumentFormat.OpenXml.Spreadsheet.Worksheet(new SheetData());
+                DocumentFormat.OpenXml.Spreadsheet.Sheets sheets = workbookPart.Workbook.AppendChild(new DocumentFormat.OpenXml.Spreadsheet.Sheets());
+                Sheet sheet = new Sheet() { Id = workbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "ListView" };
+                sheets.Append(sheet);
+
+                // Obtener la colección de celdas
+                SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+                // Recorrer las columnas y filas de la ListView
+                for (int i = 0; i < lstvDatos.Columns.Count; i++)
+                {
+                    // Crear una fila para los encabezados
+                    if (i == 0)
+                    {
+                        Row row = new Row() { RowIndex = 1 };
+                        sheetData.Append(row);
+                    }
+                    // Obtener el encabezado de la columna
+                    string headerText = lstvDatos.Columns[i].Text;
+                    // Crear una celda para el encabezado
+                    Cell headerCell = new Cell();
+                    headerCell.CellReference = GetColumnName(i + 1) + "1";
+                    headerCell.DataType = CellValues.String;
+                    headerCell.CellValue = new CellValue(headerText);
+                    
+                    // Agregar la celda al final de la fila
+                    Row headerRow = (Row)sheetData.ChildElements.GetItem(0);
+                    headerRow.AppendChild(headerCell);
+
+                    // Recorrer las filas de la columna
+                    for (int j = 0; j < lstvDatos.Items.Count; j++)
+                    {
+                        // Crear una fila para los datos
+                        if (i == 0)
+                        {
+                            Row row = new Row() { RowIndex = (uint)(j + 2) };
+                            sheetData.Append(row);
+                        }
+                        // Obtener el valor del dato
+                        string dataText = lstvDatos.Items[j].SubItems[i].Text;
+                        // Crear una celda para el dato
+                        Cell dataCell = new Cell();
+                        dataCell.CellReference = GetColumnName(i + 1) + (j + 2);
+                        dataCell.DataType = CellValues.String;
+                        dataCell.CellValue = new CellValue(dataText);
+
+                        // Agregar la celda al final de la fila
+                        Row dataRow = (Row)sheetData.ChildElements.GetItem(j + 1);
+                        dataRow.AppendChild(dataCell);
+                    }
+                }
+                // Guardar y cerrar el documento
+                workbookPart.Workbook.Save();
+                document.Close();
+                Process.Start(sfd.FileName);
+            }
+        }
+        // Método auxiliar para obtener el nombre de la columna según su índice
+        private string GetColumnName(int index)
+        {
+            int dividend = index;
+            string columnName = String.Empty;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo).ToString() + columnName;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+
+            return columnName;
+        }
+    }
+}
+
